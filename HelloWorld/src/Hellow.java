3b6420b5446be406d01cf54be603b2e43682c89b
Daniel Castellanos <vc.barroso.castellanos@lowes.com>
2013-03-12 12:27:47 -0400

    Left valid output

--- HelloWorld/src/Hellow.java
+++ HelloWorld/src/Hellow.java
@@ -5,13 +5,7 @@ public class Hellow {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Push upstream");
-		
-		System.out.println("wrong message");
-
-		System.out.println("test branch");
-		
-		System.out.println("branch from local");
+		System.out.println("valid output");
 	}
 
 }
